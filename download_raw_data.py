fb0a110df02f36de85de261b181995ecb41ddf63
Petros <psdeme@hotmail.com>
2022-12-14 01:27:57 +0100

    more raw data and updated model

--- download_raw_data.py
+++ download_raw_data.py
@@ -72,9 +72,9 @@ def morning_filenames_for_day(year, month, day):
                 prefix+'1035',prefix+'1040',prefix+'1045',prefix+'1050',prefix+'1055',prefix+'1100']
 
 #files = get_data_of_n_preevious_hours(3)
-year = '2021'
-month = '09'
-day = '30'
+year = '2022'
+month = '06'
+day = '01'
 tstamps_list = morning_filenames_for_day(year,month,day)
 dirloc = './data/raw/' + year +'-'+month + '-' + day + '-0805/'
 files = get_files_for_specific_timestamps(tstamps_list, dirloc)
