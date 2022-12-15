5968934c22e8fccb992309c2d694ec2c973a26d9
Petros <psdeme@hotmail.com>
2022-12-15 17:31:55 +0100

    added raw files

--- download_raw_data.py
+++ download_raw_data.py
@@ -73,8 +73,8 @@ def morning_filenames_for_day(year, month, day):
 
 #files = get_data_of_n_preevious_hours(3)
 year = '2019'
-month = '12'
-day = '01'
+month = '11'
+day = '15'
 tstamps_list = morning_filenames_for_day(year,month,day)
 dirloc = './data/raw/' + year +'-'+month + '-' + day + '-0805/'
 files = get_files_for_specific_timestamps(tstamps_list, dirloc)
